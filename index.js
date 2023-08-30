43377cae9005013b19524d031a22ebb91716cc32
Anson Yip Ann Yee <ansonyipannyee@outlook.com>
2023-08-30 18:10:16 +0800

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(5, 20, 10 , 15);
